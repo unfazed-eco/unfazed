6fdd08c580b1142b3231d229dbbf5085efe7d64b
xin.zhou <zhouxin576@gmail.com>
2024-09-01 23:49:35 +0800

    FIX: try solve mysql error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from tests.utils import DataBase
@@ -6,7 +8,11 @@ from tests.utils import DataBase
 @pytest.fixture(scope="session")
 def use_test_db():
     # refer: Unfazed/docker-compose.yml
-    db = DataBase("127.0.0.1", 3306, "root", "app")
+
+    host = os.getenv("MYSQL_HOST", "mysql")
+    port = int(os.getenv("MYSQL_PORT", 3306))
+    password = os.getenv("MYSQL_ROOT_PASSWORD", "app")
+    db = DataBase(host, port, "root", password)
 
     # create test database
     db.create_db("test_app")
