d707fdf06f4d95256409550ba6e733840300973c
xin.zhou <zhouxin576@gmail.com>
2024-11-11 23:58:27 +0800

    UPD: continue auth contrib

---
+++ unfazed/contrib/auth/serializers.py
@@ -0,0 +1,23 @@
+from unfazed.db.tortoise.serializer import TSerializer
+
+from .models import AbstractUser, Group, Permission, Role
+
+
+class GroupSerializer(TSerializer):
+    class Meta:
+        model = Group
+
+
+class RoleSerializer(TSerializer):
+    class Meta:
+        model = Role
+
+
+class PermissionSerializer(TSerializer):
+    class Meta:
+        model = Permission
+
+
+class UserSerializer(TSerializer):
+    class Meta:
+        model = AbstractUser.user
